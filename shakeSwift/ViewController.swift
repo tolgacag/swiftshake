fd8a8243dd062b81237c7fc2c9672ac11a71659d
Tolga <Tolga@em-tolga-cag-mac.home>
2017-05-31 18:24:57 +0300

    Initial Commit

---
+++ shakeSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  shakeSwift
+//
+//  Created by Tolga on 31/05/2017.
+//  Copyright © 2017 Tolga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
