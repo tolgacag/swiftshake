e216bf16b162cf792eb2b8504c8f17a51dd49064
Tolga <Tolga@em-tolga-cag-mac.home>
2017-05-31 19:06:55 +0300

    shakeSwift

--- shakeSwift/ViewController.swift
+++ shakeSwift/ViewController.swift
@@ -10,15 +10,51 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var imgShake: UIImageView!
+    @IBOutlet weak var lblShake: UILabel!
+    @IBOutlet weak var txtShake: UITextField!
+    @IBOutlet weak var btnShake: UIButton!
+   
+    @IBAction func btnShakeButtonPressed(_ sender: Any) {
+        shakeSwift(field: btnShake)
+    }
+    
+    @IBAction func btnShakeTextFieldPressed(_ sender: Any) {
+        shakeSwift(field: txtShake)
+    }
+    
+    @IBAction func btnShakeImagePressed(_ sender: Any) {
+        shakeSwift(field: imgShake)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func btnShakeLabelPressed(_ sender: Any) {
+      shakeSwift(field: lblShake)
     }
+    
+    @IBAction func btnShakeViewPressed(_ sender: Any) {
+        shakeSwift(field: view)
+    }
+
+    @IBAction func btnShakeAllPressed(_ sender: Any) {
+        shakeSwift(field: btnShake)
+        shakeSwift(field: txtShake)
+        shakeSwift(field: imgShake)
+        shakeSwift(field: lblShake)
+    }
+
+    
+    func shakeSwift(field: Any) {
+        
+        let shakeSwift = CABasicAnimation(keyPath: "position")
+        shakeSwift.duration = 0.07
+        shakeSwift.repeatCount = 4
+        shakeSwift.autoreverses = true
+        shakeSwift.fromValue = CGPoint(x: (field as AnyObject).center.x - 10, y: (field as AnyObject).center.y)
+        shakeSwift.toValue = CGPoint(x: (field as AnyObject).center.x + 10, y: (field as AnyObject).center.y)
+        (field as AnyObject).layer.add(shakeSwift, forKey: "position")
+        
+    }
+
 
 
 }
